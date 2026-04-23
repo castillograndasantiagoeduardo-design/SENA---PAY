5101a7589de900a8ae28ae331b296835b984d0a9
Diego Trujillo <diegoalexandertrujillomedina@gmail.com>
2026-04-23 17:28:06 -0500

    vista carnet

---
+++ Controllers/AprendizController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sena_Pay.Controllers
+{
+    public class AprendizController : Controller
+    {
+        public IActionResult Perfil()
+        {
+            // Datos de ejemplo (Hardcoded por ahora)
+            ViewBag.Nombre = "SANTIAGO ALEXANDER";
+            ViewBag.Documento = "1023456789";
+            ViewBag.Saldo = 15500;
+            ViewBag.Ficha = "2827102"; // Ejemplo de número de ficha
+
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
