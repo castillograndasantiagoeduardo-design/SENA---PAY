4db5d5e24a6e44d426e514d2803a161e2efde7db
JosephRicardo <josephfonseca2005@gmail.com>
2026-04-24 17:39:16 -0500

    Nuevo Login

---
+++ Models/LoginViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Sena_Pay.Models
+{
+    public class LoginViewModel
+    {
+        public string? Rol { get; set; }
+        public string? TipoDocumento { get; set; }
+        public string? Documento { get; set; }
+        public string? Password { get; set; }
+    }
+}
